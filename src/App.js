a1497edcfee86e09e92da3e3316b534dbbe1ff8b
RobsonTrasel <robson.trasel@gmail.com>
2022-10-13 22:40:47 -0300

    chore: add custom select menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import { Button } from "@mui/material";
+import { Button, createTheme, ThemeProvider, TextField } from "@mui/material";
+import { blue } from "@mui/material/colors";
 import React from "react";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500]
+    }
+  },
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        variant: 'outlined',
+        fullWidth: true
+      }
+    },
+    MuiSelect: {
+      defaultProps: {
+        variant: 'outlined',
+        fullWidth: true
+      }
+    }
+  }
+})
+
 const App = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <Button variant="text">Text</Button>
-    <Button variant="contained">Contained</Button>
-    <Button variant="outlined">Outlined</Button>
-  </>
+    <Button variant="contained" color="primary">primary</Button>
+    <Button variant="outlined" color="secondary">secondary</Button>
+    <TextField />
+  </ThemeProvider>
 )
 
 export default App
