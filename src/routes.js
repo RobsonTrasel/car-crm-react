70f548caf692e672543fadf3a96cf662261ea201
RobsonTrasel <robson.trasel@gmail.com>
2022-10-16 02:58:40 -0300

    fix: added .env

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import { CircularProgress } from "@mui/material";
 import React, {Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./view/Home";
+import Auth from "./view/auth";
 
 
 const RoutesPath = () => (
     <Router>
         <Suspense fallback={<div className="d-flex justify-content-center mt-5 pt-5"><CircularProgress/></div>}>
             <Routes>
-                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="/" element={<Auth/>}/>
             </Routes>
         </Suspense>
     </Router>
