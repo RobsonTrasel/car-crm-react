532e405cec8a127c49998c738132994ec65dd451
RobsonTrasel <robson.trasel@gmail.com>
2022-10-16 02:58:19 -0300

    fix: add .env to protect my client secret

---
+++ src/view/auth/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Auth() {
+  return (
+    <div className='d-flex bg-white min-vh-100'>
+        <div className='container mt-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-4'>
+                    <div className='form-group'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
